0b5c715d0e6493f5aea68002f1600d0336cf18ac
Gabriel23 <eseweyxd38@gmail.com>
2024-10-31 19:01:11 -0600

    Se le agrego  el de admin

--- app/Http/Controllers/ClienteAuthController.php
+++ app/Http/Controllers/ClienteAuthController.php
@@ -5,49 +5,50 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Cliente;
+use App\Models\Admin; 
 
 class ClienteAuthController extends Controller
 {
-    public function showForm(){
+    public function showForm()
+    {
         return view('/clientes/login');
     }
-    
-    public function login(Request $request){
 
+    public function login(Request $request)
+    {
         $credentials = $request->validate([
             'username' => ['required'],
             'password' => ['required'],
         ]);
 
-       
- 
+    
         if (Auth::guard('cliente')->attempt([
-            'username' => $request ->username,
-            'password' => $request -> password,
+            'username' => $request->username,
+            'password' => $request->password,
         ])) {
             $request->session()->regenerate();
- 
-            return redirect()->intended('/menu');
+            return redirect()->intended('/cliente/menu');
         }
- 
+
+      
+        if (Auth::guard('web')->attempt([
+            'username' => $request->username,
+            'password' => $request->password,
+        ])) {
+            $request->session()->regenerate();
+            return redirect()->intended('/cliente/menu'); 
+        }
+
         return back()->withErrors([
-            'username' => 'The provided credentials do not match our records.',
+            'username' => 'Las credenciales no coinciden con nuestros registros.',
         ])->onlyInput('username');
-
-
     }
 
-    public function logout(Request $request){
-
+    public function logout(Request $request)
+    {
         Auth::logout();
- 
-    $request->session()->invalidate();
- 
-    $request->session()->regenerateToken();
- 
-    return redirect('cliente/login');
-
-        
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('cliente/login');
     }
-
 }
